d2f126d59bd229398bb82e271f935ef5fcab0143
dkowalczyk <d.kowalczyk@dituel.pl>
2024-01-02 15:47:31 +0100

    add second project

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -2,14 +2,14 @@ export default [
   {
     title: "Coffe Shop",
     imgUrl: "/assets/coffee_shop.png",
-    stack: ["HTML", "CSS", "JavaScript"],
+    stack: ["React", "SCSS", "HTML"],
     link: "https://github.com/dominikKowalczyk17/Coffe-Shop",
   },
   {
-    title: "Coffe Shop",
-    imgUrl: "",
-    stack: ["HTML", "CSS", "JavaScript"],
-    link: "https://github.com/dominikKowalczyk17/Coffe-Shop",
+    title: "Portfolio Website",
+    imgUrl: "/assets/portfolio.png",
+    stack: ["React", "Vite", "Tailwind"],
+    link: "https://github.com/dominikKowalczyk17/Minimal-Portfolio",
   },
   {
     title: "Coffe Shop",
